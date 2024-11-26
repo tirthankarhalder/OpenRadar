e03fb01d148d4a0a450e5037868b179084f2dda4
arghasen10 <arghasen10@gmail.com>
2024-11-26 19:38:49 +0530

    data collection script

--- collect_data.py
+++ collect_data.py
@@ -1,15 +1,20 @@
 from mmwave.dataloader import DCA1000
 import time
-
+from datetime import datetime
 import sys
+import numpy as np
 
 dca = DCA1000()
 if len(sys.argv) > 1:
     num_frames = sys.argv[1]
 start_time = time.time()
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
 adc_data = dca.read(num_frames=int(num_frames))
 end_time = time.time()
 total_time = end_time-start_time
 print("Total time: ", total_time)
 print("FPS:", int(num_frames)/total_time) 
 print(adc_data.shape)
+filename = f"adc_data_{current_time}.npy"
+np.save(filename, adc_data)
